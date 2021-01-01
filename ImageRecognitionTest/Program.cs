54165fcd100a7f05891d8fc66ba8fa987c5c0ad7
Pontus Magnusson <pontuspdmagnusson@gmail.com>
2021-01-01 23:52:12 +0100

    Unscuff code by 3%

--- ImageRecognitionTest/Program.cs
+++ ImageRecognitionTest/Program.cs
@@ -1,90 +1,90 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
 using System.IO;
 
 namespace ImageRecognitionTest
 {
-    class Program
+    /// <summary>
+    /// Code below detects a single colored circle with the width of 1 pixel in a picture.
+    /// The code iterates until it finds the first pixel in a circle while iterating each x pixel in y row.
+    /// When pixel is found, we know its the top row of the circle, we can therefore iterate down to the bottom
+    /// of the circle to determine center y coordinate and radius/diameter.
+    /// While at the bottom we measure the bottom row in pixels and with that info we can get the x coordinate.
+    /// </summary>
+    static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             CreateImage();
 
-            // Load image file
-            using (var bmp = new Bitmap("image.png"))
+            using (var image = new Bitmap("image.png"))
             {
-                /* Code below detects a single colored circle with the width of 1 pixel in a picture.
-                 * The code iterates until it finds the first pixel in a circle while iterating each x pixel in y row.
-                 * When pixel is found, we know its the top row of the circle, we can therefore iterate down to the bottom
-                 * of the circle to determine center y coordinate and radius/diameter.
-                 * While at the bottom we measure the bottom row in pixels and with that info we can get the x coordinate.
-                 */
+                Color backgroundColor = image.GetPixel(0, 0);
+                bool foundAnomaly = false;
 
-                Color c = bmp.GetPixel(0, 0); // Get the first pixel
-                bool foundAnomaly = false; // Flag if we found a circle. Program can only detect one circle.
-
-                // Iterate through the image pixels
-                for (int y = 0; y < bmp.Size.Height; y++)
+                for (int y = 0; y < image.Size.Height; y++)
                 {
-                    for (int x = 0; x < bmp.Size.Width; x++)
+                    for (int x = 0; x < image.Size.Width; x++)
                     {
-                        if (c != bmp.GetPixel(x, y)) // Is the previous pixel a different color from the current?
+                        if (backgroundColor != image.GetPixel(x, y))
                         {
                             foundAnomaly = true;
-                            Color? anomalyColor = bmp.GetPixel(x, y);
-                            float yCoord = 0;
-
-                            // Find radius and vertical center coordinate
-                            float radius = 0;
-                            int allowedLineThickness = 15; // Also the minimum diameter
-                            for (int h = y + allowedLineThickness; h < bmp.Size.Height; h++)
-                            {
-                                if (anomalyColor == bmp.GetPixel(x, h))  // Detects color anomaly
-                                {
-                                    radius = (h - y) / (float)2; // radius is half the diameter hence divide by 2
-
-                                    // Find the vertical center
-                                    yCoord = y + radius;
-
-                                    break; // We dont need to iterate more
-                                }
-                            }
-
-                            // Find the horizontal center coordinate
-                            float xCoord = 0;
-                            for (int w = x; w < bmp.Size.Width; w++)
-                            {
-                                if (anomalyColor != bmp.GetPixel(w, y)) // Detects color anomaly
-                                {
-                                    xCoord = x + ((w - x) / 2);
-                                    break; // We dont need to iterate more
-                                }
-                            }
-
-                            // Draw dot in center of circle
-                            using (var gr = Graphics.FromImage(bmp))
-                            {
-                                gr.FillRectangle(Brushes.Blue, xCoord, yCoord, 1, 1); // DrawRectangle draws 2 pixels minimum, FillRectangle draws 1 pixel
-                            }
+                            Color? anomalyColor = image.GetPixel(x, y);
+                            float centerY = FindVerticalCenter(image, y, x, anomalyColor);
+                            float centerX = FindHorizontalCenter(image, y, x, anomalyColor);
+                            MarkCenter(image, centerY, centerX);
                         }
 
-                        c = bmp.GetPixel(x, y); // Get the current pixel
-                        if (foundAnomaly) break; // Program can only detect 1 circle
+                        backgroundColor = image.GetPixel(x, y);
+                        if (foundAnomaly) break;
                     }
-                    if (foundAnomaly) break; // Program can only detect 1 circle
+                    if (foundAnomaly) break;
                 }
 
-                // Save the image to "out.png"
-                var path = "out.png";
-                bmp.Save(path);
+                image.Save("result.png");
             }
 
             Console.WriteLine("Process completed.");
-            Console.ReadLine();
+        }
+
+        private static void MarkCenter(Bitmap bmp, float yCoord, float xCoord)
+        {
+            using (var gr = Graphics.FromImage(bmp))
+            {
+                gr.FillRectangle(Brushes.Blue, xCoord, yCoord, 2, 2);
+            }
+        }
+
+        private static float FindHorizontalCenter(Bitmap bmp, int y, int x, Color? anomalyColor)
+        {
+            float xCoord = 0;
+            for (int w = x; w < bmp.Size.Width; w++)
+            {
+                if (anomalyColor != bmp.GetPixel(w, y))
+                {
+                    xCoord = x + (w - x) / 2;
+                    break;
+                }
+            }
+
+            return xCoord;
+        }
+
+        private static float FindVerticalCenter(Bitmap bmp, int y, int x, Color? anomalyColor)
+        {
+            float yCoord = 0;
+            int allowedLineThickness = 15;
+            for (int h = y + allowedLineThickness; h < bmp.Size.Height; h++)
+            {
+                if (anomalyColor == bmp.GetPixel(x, h))
+                {
+                    float radius = (h - y) / (float)2;
+                    yCoord = y + radius;
+                    break;
+                }
+            }
+
+            return yCoord;
         }
 
         private static void CreateImage()
@@ -100,8 +100,7 @@ namespace ImageRecognitionTest
                         gr.DrawEllipse(Pens.Red, 10, 10, 80, 80);
                     }
 
-                    var path = "image.png";
-                    bmp.Save(path);
+                    bmp.Save("image.png");
                 }
             }
         }
